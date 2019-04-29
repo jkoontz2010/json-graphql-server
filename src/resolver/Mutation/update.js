c8af2c55a8f3d0987fbed90a3e4d0b9ccccff3f7
Jay Koontz <jay@wonolo.com>
2019-04-29 11:27:24 -0700

    flatten attributes

--- src/resolver/Mutation/update.js
+++ src/resolver/Mutation/update.js
@@ -3,12 +3,15 @@ export default (entityData = []) => (_, params) => {
     const indexOfEntity = entityData.findIndex(
         e => parseInt(e.id, 10) === parsedId
     );
-    console.log("UPDATE MUTATION", params, "DATA", entityData[indexOfEntity])
+    const paramsWithoutAttributes = params.attributes
+        ? Object.assign({}, params, ...params.attributes)
+        : params;
+    console.log('UPDATE MUTATION', paramsWithoutAttributes, 'DATA', entityData[indexOfEntity]);
     if (indexOfEntity !== -1) {
         entityData[indexOfEntity] = Object.assign(
             {},
             entityData[indexOfEntity],
-            params
+            paramsWithoutAttributes
         );
         return entityData[indexOfEntity];
     }
