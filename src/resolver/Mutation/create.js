30fa041c100055f977112839256b470c75460cff
Jay Koontz <jay@wonolo.com>
2019-04-29 13:44:34 -0700

    uses omit to exclude attributes

--- src/resolver/Mutation/create.js
+++ src/resolver/Mutation/create.js
@@ -1,6 +1,8 @@
+import omit from 'lodash.omit';
+
 export default (entityData = []) => (_, entity) => {
     const entityWithoutAttributes = entity.attributes
-        ? Object.assign({}, entity.attributes)
+        ? omit(Object.assign(entity, entity.attributes), 'attributes')
         : entity;
     const newId =
         entityData.length > 0 ? entityData[entityData.length - 1].id + 1 : 0;
--- src/resolver/Mutation/update.js
+++ src/resolver/Mutation/update.js
@@ -1,10 +1,12 @@
+import omit from 'lodash.omit';
+
 export default (entityData = []) => (_, params) => {
     const parsedId = parseInt(params.id, 10); // FIXME fails for non-integer ids
     const indexOfEntity = entityData.findIndex(
         e => parseInt(e.id, 10) === parsedId
     );
     const paramsWithoutAttributes = params.attributes
-        ? Object.assign({}, { id: params.id }, params.attributes)
+        ? omit(Object.assign({}, params, params.attributes), 'attributes')
         : params;
     if (indexOfEntity !== -1) {
         entityData[indexOfEntity] = Object.assign(
