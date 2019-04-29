df11fdb470d6eee0a262a4b15c77c9706e4d427f
Jay Koontz <jay@wonolo.com>
2019-04-29 11:49:51 -0700

    attribute generalization reversed, used in specific create and update mutation resolvers

--- src/resolver/Mutation/create.js
+++ src/resolver/Mutation/create.js
@@ -1,7 +1,10 @@
 export default (entityData = []) => (_, entity) => {
+    const entityWithoutAttributes = entity.attributes
+        ? Object.assign({}, entity.attributes)
+        : entity;
     const newId =
         entityData.length > 0 ? entityData[entityData.length - 1].id + 1 : 0;
-    const newEntity = Object.assign({ id: newId }, entity);
+    const newEntity = Object.assign({ id: newId }, entityWithoutAttributes);
 
     entityData.push(newEntity);
     return newEntity;
--- src/resolver/Mutation/update.js
+++ src/resolver/Mutation/update.js
@@ -3,12 +3,14 @@ export default (entityData = []) => (_, params) => {
     const indexOfEntity = entityData.findIndex(
         e => parseInt(e.id, 10) === parsedId
     );
-
+    const paramsWithoutAttributes = params.attributes
+        ? Object.assign({}, { id: params.id }, params.attributes)
+        : params;
     if (indexOfEntity !== -1) {
         entityData[indexOfEntity] = Object.assign(
             {},
             entityData[indexOfEntity],
-            params
+            paramsWithoutAttributes
         );
         return entityData[indexOfEntity];
     }
