fe471b4cb3a577489fd6a0bb85a162af37b248e6
Jay Koontz <jay@wonolo.com>
2019-04-29 11:36:24 -0700

    attributes round 3

--- src/resolver/Mutation/update.js
+++ src/resolver/Mutation/update.js
@@ -4,7 +4,7 @@ export default (entityData = []) => (_, params) => {
         e => parseInt(e.id, 10) === parsedId
     );
     const paramsWithoutAttributes = params.attributes
-        ? Object.assign({}, { id: params.id }, ...params.attributes)
+        ? Object.assign({}, { id: params.id }, params.attributes)
         : params;
     if(params.attributes) console.log('has attributes')
     console.log('UPDATE MUTATION', paramsWithoutAttributes, 'DATA', entityData[indexOfEntity]);
