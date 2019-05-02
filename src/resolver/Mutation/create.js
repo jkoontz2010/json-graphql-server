5f5888b93ad2596c2ebc80dfc8197b927397b0e9
Jay Koontz <jay@wonolo.com>
2019-05-02 10:55:42 -0700

    another max id approach

--- src/resolver/Mutation/create.js
+++ src/resolver/Mutation/create.js
@@ -2,14 +2,16 @@ import omit from 'lodash.omit';
 
 const getMaxId = collection => {
     return collection.reduce(function(prev, current) {
-        return prev.id > current.id ? prev : current;
+        const prevId = prev.id ? +prev.id : 0;
+        const currentId = current.id ? +current.id : 0;
+        return prevId > currentId ? prev : current;
     });
 };
 export default (entityData = [], entityName) => (_, entity) => {
     const entityWithoutAttributes = entity.attributes
         ? omit(Object.assign(entity, entity.attributes), 'attributes')
         : entity;
-    const newId = entityData.length > 0 ? +getMaxId(entityData).id + 1 : 0;
+    const newId = entityData.length + +getMaxId(entityData).id;
     const newEntity = Object.assign(entityWithoutAttributes, { id: newId });
     entityData.push(newEntity);
     return { [entityName]: newEntity };
